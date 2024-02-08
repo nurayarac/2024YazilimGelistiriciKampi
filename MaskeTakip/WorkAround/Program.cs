d86722866bb9ac64ab1fa4dfc075f846031e7dfc
nurayarac <nurayarac19@gmail.com>
2024-02-08 19:13:11 +0300

    ödev-2

---
+++ MaskeTakip/WorkAround/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Workaround
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            SelamVer(isim : "Nuray");
+            SelamVer(isim: "derya");
+            SelamVer(isim: "zehra");
+            SelamVer();
+            int sonuc = Topla(3,5);
+            Console.ReadLine();
+        }
+
+        static void SelamVer(string isim = "İsimsiz")
+        {
+            Console.WriteLine("Merhaba" + isim);
+        }
+
+        static int Topla(int sayi1 , int sayi2)
+        {
+            int sonuc = sayi1 + sayi2;
+            Console.WriteLine("Toplam :" + sonuc.ToString());
+            return sonuc;
+        }
+    }
+    
+}
