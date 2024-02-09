186779ee52b3a8d4d7a6e9ef7ed44f77a24a5303
Nuray Araç <77547437+nurayarac@users.noreply.github.com>
2024-02-09 17:47:54 +0300

    Update PersonManager.cs

--- MaskeTakip/Business/Concrete/PersonManager.cs
+++ MaskeTakip/Business/Concrete/PersonManager.cs
@@ -18,5 +18,15 @@ namespace Business.Concrete
         {
             return null;
         }
+        public bool CheckPerson(Person person)
+{
+    KPSPublicSoapClient client = new KPSPublicSoapClient(KPSPublicSoapClient.EndpointConfiguration.KPSPublicSoap);
+
+    return client.TCKimlikNoDogrulaAsync(
+        new TCKimlikNoDogrulaRequest(
+            new TCKimlikNoDogrulaRequestBody(person.NationalIdenty , Ad: person.FirstName , Soyad : person.LastName , DogumYili : person.DateOfBirthYear)))
+            .Result.Body.TCKimlikNoDogrulaResult;
+   
+}
     }
 }
